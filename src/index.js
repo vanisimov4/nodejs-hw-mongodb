aea4d1ffd4ee273ef79027dc7019fe34bbf112dd
Volodymyr Anisimov <144102084+vanisimov4@users.noreply.github.com>
2025-07-05 20:02:09 +0300

    setupServer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
-import express from 'express';
+import { setupServer } from './server.js';
 
-const app = express();
+setupServer();
 
-const PORT = 3000;
+// import express from 'express';
+// import pino from 'pino-http';
+// import cors from 'cors';
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = 3000;
+
+// const app = express();
+
+// app.use(cors());
+
+// app.use(
+//   pino({
+//     transport: {
+//       target: 'pino-pretty',
+//     },
+//   }),
+// );
+
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'Hello world!',
+//   });
+// });
+
+// app.use((req, res, next) => {
+//   res.status(404).json({
+//     message: 'Not found',
+//   });
+// });
+
+// app.use((err, req, res, next) => {
+//   res.status(500).json({
+//     message: 'Something went wrong',
+//     error: err.message,
+//   });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
--- src/server.js
+++ src/server.js
@@ -1,9 +1,42 @@
-// import express from 'express';
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
 
-// const app = express();
+const PORT = 3000;
 
-// const PORT = 3000;
+export const setupServer = () => {
+  const app = express();
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
