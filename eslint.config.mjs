bb8c67bf76f51139d1c932336045bb05d738781a
Volodymyr Anisimov <144102084+vanisimov4@users.noreply.github.com>
2025-07-06 14:07:01 +0300

    finish

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,3 @@
-// import js from '@eslint/js';
-// import globals from 'globals';
-// import { defineConfig } from 'eslint/config';
-
-// export default defineConfig([
-//   {
-//     files: ['**/*.{js,mjs,cjs}'],
-//     plugins: { js },
-//     extends: ['js/recommended'],
-//   },
-//   { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
-// ]);
-
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 
--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-// import dotenv from 'dotenv';
 
 import { getEnvVar } from './utils/getEnvVar.js';
-
-// dotenv.config();
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -26,6 +24,36 @@ export const setupServer = () => {
   app.get('/', (req, res) => {
     res.json({
       message: 'Hello world!',
+    });
+  });
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    // Відповідь, якщо контакт не знайдено
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    // Відповідь, якщо контакт знайдено
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
     });
   });
 
